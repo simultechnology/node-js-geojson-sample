3e184087b056caccfb172fce3090b791954d4a4f
t_ishikawa <t_ishikawa@zenrin-datacom.net>
2018-07-13 12:21:09 +0900

    ズーム出力処理の追加

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ utils.callReadFile('./data/shiteikasen.json')
   .then((content) => {
     let geojson = JSON.parse(content.toString())
     console.log(geojson)
-    const buf = [];
+    const buf = []
     geojson.features.forEach((feature) => {
       const geom = turf.getGeom(feature)
-      const bbox = turf.bbox(geom);
+      const bbox = turf.bbox(geom)
+      const zoom = utils.getAdjustZoom(
+        {lat: bbox[1], lon: bbox[0]},
+        {lat: bbox[3], lon: bbox[2]},
+        662) // PCは310, spは662
       buf.push({
         river_no: feature.properties.RIVER_NO,
         name: feature.properties.FRCST_AREA,
-        bbox: bbox
+        bbox: bbox,
+        zoom: zoom
       })
     })
     return buf
